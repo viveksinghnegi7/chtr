ec9f3f21334b6ef9a4002f98692ac1fe2d9a00a1
Tobias Moe Thorstensen <tobias.thorstensen@gmail.com>
2018-11-29 16:02:54 +0100

    Added Chathub

---
+++ backend/chtr.server/chtr.server/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace chtr.server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task NotifyNewUser(string userName)
+        {
+            return Clients.All.SendAsync("NotifyNewUser", userName);
+        }
+    }
+}
--- backend/chtr.server/chtr.server/Startup.cs
+++ backend/chtr.server/chtr.server/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using chtr.server.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -30,6 +31,11 @@ namespace chtr.server
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSignalR(cfg =>
+            {
+                cfg.MapHub<ChatHub>("/Chat");
+            });
         }
     }
 }
